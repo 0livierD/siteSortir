7be242f76375ac48a87b24863efdb355f93098a7
ody2023 <olivier.dy2023@campus-eni.fr>
2023-11-14 17:00:32 +0100

    fin de journée / maj CSS

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,7 +13,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
+            ->add('Password', 'attr')
             ->add('nom')
             ->add('prenom')
             ->add('pseudo')
