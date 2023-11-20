324c5987c4ebc82065fd8307da8f67b531258c2e
Yves-Alexis LORVELLEC <yvesalexis.lorvellec2023@campus-eni.fr>
2023-11-20 13:40:02 +0100

    Mise a jour edition User sur le dashboard Admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Site;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +18,23 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            EmailField::new('email'),
+            TextField::new('pseudo'),
+            TextField::new('telephone'),
+            TextField::new('password'),
+            TextField::new('photo'),
+            AssociationField::new('site')
+            ->setFormTypeOption('class', Site::class)
+                ->setFormTypeOption('choice_label', 'nom'),
+            BooleanField::new('isAdministrateur'),
+            BooleanField::new('isActif'),
         ];
     }
-    */
+
 }
