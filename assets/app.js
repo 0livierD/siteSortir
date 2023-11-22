67a182b22da55227ea99f7e202adac0c7eedf340
Yves-Alexis LORVELLEC <yvesalexis.lorvellec2023@campus-eni.fr>
2023-11-22 11:22:56 +0100

    Correction route/js lieux + reprise  style

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,6 @@
 import './styles/app.css';
 
 document.addEventListener('DOMContentLoaded', function() {
-
     var selectVille = document.getElementById('select-ville');
     var selectLieu = document.getElementById('sortie_lieu');
 
@@ -17,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
         var selectedVilleId = this.value;
 
         // Effectuez une requête AJAX pour récupérer les lieux
-        fetch('/getLieuxByVille/' + selectedVilleId)
+        fetch('/lieux-par-villes/' + selectedVilleId)
             .then(response => response.json())
             .then(data => {
                 // Mettez à jour la liste des lieux si l'élément select-lieu existe
                 if (selectLieu) {
-                    selectLieu.innerHTML = '';
+                    selectLieu.innerHTML = ''; // Efface les anciennes options
                     data.forEach(lieu => {
                         var option = document.createElement('option');
                         option.value = lieu.id;
@@ -30,36 +29,12 @@ document.addEventListener('DOMContentLoaded', function() {
                         selectLieu.appendChild(option);
                     });
                 } else {
-                    console.error('L\'élément avec l\'id "select-lieu" n\'a pas été trouvé dans le DOM.');
+                    console.error('L\'élément avec l\'id "sortie_lieu" n\'a pas été trouvé dans le DOM.');
                 }
             })
             .catch(error => {
+
                 console.error('Erreur lors de la récupération des lieux:', error);
             });
     });
-    document.getElementById('btn-publier').addEventListener('click', function() {
-        var form = document.querySelector('form');
-        var formData = new FormData(form);
-
-        // Effectuez une requête AJAX pour mettre à jour le statut
-        fetch('/miseAjourStatut', {
-            method: 'POST',
-            body: formData,
-        })
-    .then(response => {
-            if (response.ok) {
-                console.log('Statut mis à jour avec succès');
-                // Ajoutez ici tout autre traitement nécessaire après la mise à jour du statut
-            } else {
-                console.error('Erreur lors de la mise à jour du statut');
-            }
-        })
-            .catch(error => {
-                console.error('Erreur lors de la requête AJAX', error);
-            });
-    });
-
-
 });
-
-
